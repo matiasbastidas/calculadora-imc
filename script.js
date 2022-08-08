43864a50aa377ef3b6a58f787b9756473190cd9b
matiasbastidas <matias.bastidasm@gmail.com>
2022-08-08 17:08:26 -0400

    agregado obesidad

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
-function calcularIMC() {
-    let peso = document.getElementById('peso').value
-    let altura = document.getElementById('altura').value
+const calcularIMC = () => {
+    peso = document.getElementById('peso').value
+    // calcular la altura cm a m
+    altura = (document.getElementById('altura').value) / 100
 
-    let imc = peso * (altura * altura)
+    imc = peso / (Math.pow(altura, 2))
 
     document.getElementById('encabezado').innerHTML = 'Tu IMC es:'
     document.getElementById('imc-resultado').innerHTML = imc.toFixed(2)
 
-    if (imc <= 24.9) {
+    if (imc <= 18.5) {
         document.getElementById('mensaje').innerHTML = 'Estas bajo el peso'
-    } else if (imc >= 25 && imc <= 29.9) {
-        document.getElementById('mensaje').innerHTML = 'Estas sano'
+    } else if (imc > 18.5 && imc <= 24.9) {
+        document.getElementById('mensaje').innerHTML = 'Estas con el peso normal'
+    } else if (imc > 24.9 && imc <= 29.9) {
+        document.getElementById('mensaje').innerHTML = 'Estas con sobrepeso'
     } else {
-        document.getElementById('mensaje').innerHTML = 'Estas con sobre peso'
+        document.getElementById('mensaje').innerHTML = 'Estas con obesidad'
     }
+
+    console.log(peso, altura, imc)
 }
 
-function recargar() {
+const recargar = () => {
     window.location.reload()
 }
+
+
+
+
+
+
